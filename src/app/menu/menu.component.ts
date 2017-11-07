724a21236915b4c19b880e3b906e4bc25a98751e
Diego Rendón <desarrolloingenieria1@udea.edu.co>
2017-11-07 10:01:57 -0500

    HTTP part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 
 import { Dish } from '../shared/dish';
-
 import { DishService } from '../services/dish.service';
 
 @Component({
@@ -12,13 +11,15 @@ import { DishService } from '../services/dish.service';
 export class MenuComponent implements OnInit {
 
   dishes: Dish[];
+  errMess: string;
 
   constructor(private dishService: DishService,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
     this.dishService.getDishes()
-      .subscribe(dishes => this.dishes = dishes);
+      .subscribe(dishes => this.dishes = dishes,
+        errmess => this.errMess = <any>errmess);
   }
 
 }
